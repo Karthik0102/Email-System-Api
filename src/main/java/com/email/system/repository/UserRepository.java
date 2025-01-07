6c73586294c40d0f26924d313bc518999e80c756
Karthik <Karthikgoud0102@gmail.com>
2025-01-07 18:45:06 -0500

    Remove smtp.properties from tracking

--- src/main/java/com/email/system/repository/UserRepository.java
+++ src/main/java/com/email/system/repository/UserRepository.java
@@ -1,11 +1,11 @@
-package com.email.system.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.email.system.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-	
-	User findByUserName(String userName);
-
-}
+//package com.email.system.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import com.email.system.model.User;
+//
+//public interface UserRepository extends JpaRepository<User, Long> {
+//	
+//	User findByUserName(String userName);
+//
+//}
